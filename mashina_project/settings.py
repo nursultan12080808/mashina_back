4055b84974cf8cfc56e26215d2c4b9b359df2b10
nursultan12080808 <nursultan.top.game@gmail.com>
2024-07-10 13:34:10 +0600

    commit

--- mashina_project/settings.py
+++ mashina_project/settings.py
@@ -86,14 +86,21 @@ WSGI_APPLICATION = 'mashina_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cardb',
+#         'USER': 'nursultan',
+#         'PASSWORD': 'nur120808',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cardb',
-        'USER': 'nursultan',
-        'PASSWORD': 'nur120808',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
